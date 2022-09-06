6d56ca168141269ae5b63a92dd7b8b58ca90b6cb
philmarcu <philmark18@gmail.com>
2022-09-06 14:20:09 -0600

    Refactor - add param check for users#create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(user_params)
-    redirect_to "/users/#{User.last.id}"
+    new_user = User.create(user_params)
+    if params[:name].empty? || params[:email].empty? || params[:password].empty?
+      flash[:alert] = "Error - #{new_user.errors.full_messages}"
+      redirect_to "/users/#{User.last.id}"
+    elsif params[:password] != params[:password_confirmation]
+      flash[:alert] = "Error - Passwords must match"
+      redirect_to "/users/#{User.last.id}"
+    else
+      flash[:success] = "Welcome #{new_user.name}"
+      redirect_to "/users/#{User.last.id}"
+    end
   end
 
   private
 
   def user_params
-    params.permit(:name, :email, :password)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -10,12 +10,36 @@ RSpec.describe 'register page page' do
     visit '/register'
   end
 
-  it 'can register a user by unique email' do
-    fill_in 'Name', with: 'Tyler'
-    fill_in 'Email', with: 'tyler@user.com'
-    fill_in 'Password', with: 'pwd'
-    fill_in 'Password Confirmation', with: 'pwd'
-    click_on('Submit')
-    expect(current_path).to eq("/users/#{User.last.id}")
+  context 'happy path' do
+    it 'can register a user by unique email' do
+      fill_in 'Name', with: 'Tyler'
+      fill_in 'Email', with: 'tyler@user.com'
+      fill_in 'Password', with: 'pwd'
+      fill_in 'Password Confirmation', with: 'pwd'
+      click_on('Submit')
+      expect(current_path).to eq("/users/#{User.last.id}")
+    end
+  end
+
+  context 'sad path' do
+    it 'displays an error message if user fails to fill in parameters' do
+      fill_in 'Name', with: nil
+      fill_in 'Email', with: 'tater@gmail.com'
+      fill_in 'Password', with: 'pwd'
+      fill_in 'Password Confirmation', with: 'pwd'
+      click_on('Submit')
+
+      expect(page).to have_content("Name can't be blank")
+    end
+
+    it 'displays error message if passwords do not match' do
+      fill_in 'Name', with: "yep"
+      fill_in 'Email', with: 'tater@gmail.com'
+      fill_in 'Password', with: 'pwd'
+      fill_in 'Password Confirmation', with: 'wrong_pwd'
+      click_on('Submit')
+
+      expect(page).to have_content("Error - Passwords must match")
+    end
   end
 end
